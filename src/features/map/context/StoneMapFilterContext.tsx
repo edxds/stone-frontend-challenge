0eddb5b3fae7b5f7be8dbeac3c30f4e3ad22e480
Eduardo Souza <edux404@gmail.com>
2020-11-09 15:37:20 -0300

    feat: Introduce StoneMap filter context

---
+++ src/features/map/context/StoneMapFilterContext.tsx
@@ -0,0 +1,34 @@
+import React, { useContext, useState } from 'react';
+
+import { StoneMapMarkerType } from '../markers';
+
+export type StoneMapFilterContextValue = StoneMapMarkerType | 'ALL';
+
+export type StoneMapFilterContextState = {
+  filter: StoneMapFilterContextValue;
+  setFilter(value: StoneMapFilterContextValue): void;
+};
+
+export const StoneMapFilterValues: StoneMapFilterContextValue[] = [
+  'CLIENT',
+  'PROPOSAL',
+  'QUALIFICATION',
+  'ALL',
+];
+
+export const StoneMapFilterContext = React.createContext<StoneMapFilterContextState>({
+  filter: 'ALL',
+  setFilter: () => void 0,
+});
+
+export const useStoneMapFilter = () => useContext(StoneMapFilterContext);
+
+export function StoneMapFilterContextProvider({ children }: { children: React.ReactNode }) {
+  const [filter, setFilter] = useState<StoneMapFilterContextValue>('ALL');
+
+  return (
+    <StoneMapFilterContext.Provider value={{ filter, setFilter }}>
+      {children}
+    </StoneMapFilterContext.Provider>
+  );
+}
